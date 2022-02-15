edefb73ad7c5c5b0d7135a15a395c9235a9ac849
Manoj Kukreja <mkukreja1@gmail.com>
2022-02-15 09:31:52 -0500

    cdc blog

--- cdc-source/pre-req.sh
+++ cdc-source/pre-req.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 stty -echo
+echo "Starting API Layer"
 sh start_api.sh 
 aws ec2 delete-security-group --group-name "RDS Security Group" > /dev/null
+echo "Creating Kinesis Stream"
 aws kinesis create-stream --stream-name hotelStream --shard-count 1  > /dev/null            
+echo "Creating RDS Database"
 RDS_GROUP=`aws ec2 create-security-group --description sg-rds --group-name "RDS Security Group" | grep GroupId | sed 's/"GroupId"://' | sed 's/"//g' | sed 's/,//g'`;echo $RDS_GROUP > /dev/null
 aws ec2 authorize-security-group-ingress --group-id $RDS_GROUP  --protocol tcp --port 3306 --cidr 0.0.0.0/0 > /dev/null
 aws rds create-db-instance --db-instance-identifier hotelcdc --db-instance-class db.t2.micro --engine mysql --region us-east-1 --output text --master-username admin --master-user-password admin123 --allocated-storage 20 --vpc-security-group-ids $RDS_GROUP --db-parameter-group-name default.mysql8.0 --option-group-name default:mysql-8-0 --engine-version 8.0.27 > /dev/null
